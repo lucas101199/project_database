353b7a206377b93809890b357876b5927577e618
Ask Morken <ask.norheim@gmail.com>
2019-10-08 17:34:11 +0200

    Added .gitignore and some other files

---
+++ new.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<div class="container" id="main-content">
+	<h2>Create transaction</h2>
+    <p>
+        From account: *Choose between own accounts in dropdown
+    </p>
+    <p>
+        To account:
+        <textarea rows="1" cols="30">
+            Amount
+        </textarea>
+    </p>
+	<p>
+        Amount:
+        <textarea rows="1" cols="30">
+            Amount
+        </textarea>
+	</p>
+    <p>
+        Currency: *Dropdown
+    </p>
+    <p>
+        <button type="button" onclick="alert('Hello world!')">Click Me!</button>
+    </p>
+</div>
+
+</body>
+</html>
