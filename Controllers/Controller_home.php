cb4147db0e4d0ee5e32a120e0690c9562240b6c1
lucas <lucas_fernandez@orange.fr>
2019-10-09 04:39:23 +0200

    add controller home

---
+++ Controllers/Controller_home.php
@@ -0,0 +1,11 @@
+<?php
+class Controller_home extends Controller{
+
+    public function action_default() {
+        $this->action_home_page();
+    }
+
+    public function action_home_page() {
+        $this->render("login");
+    }
+}
